f8ba4510fd779fd72398d05502ace7ab78bd399e
vic <sapitopicador@gmail.com>
2020-06-11 16:24:10 -0500

    se sube el anvanza de las actividades

--- Analisis/Utilidades.sql
+++ Analisis/Utilidades.sql
@@ -1,3 +1,27 @@
+DELETE FROM [dbo].[InventarioDetalle]
+DBCC CHECKIDENT ('InventarioDetalle', RESEED, 0)
+DELETE FROM [dbo].[Inventario]
+DBCC CHECKIDENT ('Inventario', RESEED, 0)
+DELETE FROM  PRODUCTOS
+DBCC CHECKIDENT ('PRODUCTOS', RESEED, 0)
+
+
+
+
+
+
+/**************	INSERT DE CATALOGO TIPO INV**************************/
+truncate table CatTipoInventario
+DBCC CHECKIDENT ('CatTipoInventario', RESEED, 0)
+
+insert into CatTipoInventario  (descripcion) values ('No registrado')
+insert into CatTipoInventario  (descripcion) values ('Registrado')
+insert into CatTipoInventario  (descripcion) values ('Entrada')
+insert into CatTipoInventario  (descripcion) values ('Salida')
+insert into CatTipoInventario  (descripcion) values ('Re-localizado')
+select * from CatTipoInventario
+
+/**************************************************************/
  exec SP_AGREGA_ACTUALIZA_USUARIO
  @idUsuario = 7,
 @nombreCompleto = 'Victor Adrian Reyes',
@@ -16,10 +40,12 @@ delete from Inventario
 DBCC CHECKIDENT ('Inventario', RESEED, 0)
 delete from productos
 DBCC CHECKIDENT ('productos', RESEED, 0)
+
 exec SP_AGREGA_ACTUALIZA_PRODUCTO
 @idProducto = 0,
-@tag ='QWER123QWER-PEPINOS',
+@tag ='666482-PEPINOS',
 @descripcion ='PEPINOS'
+
 select * from Productos
 select * from Inventario
 select * from InventarioDetalle
@@ -32,10 +58,11 @@ exec SP_ACTUALIZA_ACITVO_PRODUCTO
 exec SP_OBTENER_PRODUCTOS 4
 /******AFECTA INVENTARIO********************/
 exec SP_AFECTA_INVENTARIO
-@tagId = 'PEPINO_1'
-,@cantidad = 250
+ @tagId = 'QWER123-PEPINOS'
+,@cantidad = 125
 ,@TipoInventario = 1
 ,@noPuerta = 1
+
 select * from Productos
 select * from Inventario
 select * from InventarioDetalle
